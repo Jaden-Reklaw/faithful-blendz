27100c98593566ce5789d3531be3d9c957d410ad
Jordan Walker <jwalker117@gmail.com>
2021-03-03 13:20:41 -0600

    initial commit

---
+++ src/main/java/com/fb/faithfulblendzbe/FaithfulblendzBeApplication.java
@@ -0,0 +1,13 @@
+package com.fb.faithfulblendzbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaithfulblendzBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaithfulblendzBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fb/faithfulblendzbe/FaithfulblendzBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fb.faithfulblendzbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaithfulblendzBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
